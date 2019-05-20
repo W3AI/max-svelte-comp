bf3ba15a3ad2d934602f5590f43badf97dc30d33
stefian <v_ianta@yahoo.com>
2019-05-20 13:53:17 -0400

    added 2nd listener to Service comp

--- src/Service.svelte
+++ src/Service.svelte
@@ -2,8 +2,8 @@
     export let serviceTitle;
 </script>
 
-<article on:click="{() => console.log('Jo')}">
+<article>
     <h1>{serviceTitle}</h1>
     <button on:click>Add to Cart</button>
-    <button>Delete</button>
+    <button on:click>Delete</button>
 </article>
