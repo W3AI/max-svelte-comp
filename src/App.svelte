eb5f8775ada843e7cddef90ec69a3cb0bb9ffa11
stefian <v_ianta@yahoo.com>
2019-05-20 14:19:30 -0400

    added 2 functions in parent comp to catch event data

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
   import Service from "./Service.svelte";
+
+  function addToCart(event) {
+      console.log(event);
+  }
+    function deleteService(event) {
+      console.log(event);
+  }
 </script>
 
 <Service
   serviceTitle="A Booking"
-  on:add-to-cart={() => alert('Add to cart!')}
-  on:delete={() => {
-    alert('Delete!');
-  }} />
+  on:add-to-cart={addToCart}
+  on:delete={deleteService} />
