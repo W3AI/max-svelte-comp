c14bf459155c960daed1b81ff0bf3ce7173494f9
stefian <v_ianta@yahoo.com>
2019-05-20 14:12:48 -0400

    added createEventDispatcher and 2 custom events

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
-    import Service from './Service.svelte';
+  import Service from "./Service.svelte";
 </script>
 
-<Service serviceTitle="A Booking" on:click="{() => alert('Clicked!')}" />
+<Service
+  serviceTitle="A Booking"
+  on:add-to-cart={() => alert('Add to cart!')}
+  on:delete={() => {
+    alert('Delete!');
+  }} />
--- src/Service.svelte
+++ src/Service.svelte
@@ -1,9 +1,17 @@
 <script>
+    import { createEventDispatcher } from 'svelte';
+
     export let serviceTitle;
+
+    const dispatch = createEventDispatcher();
+
+    function addToCart() {
+        dispatch('add-to-cart', {id: 's1'}); // can pass context graph ids !!!
+    }
 </script>
 
 <article>
     <h1>{serviceTitle}</h1>
-    <button on:click>Add to Cart</button>
-    <button on:click>Delete</button>
+    <button on:click="{addToCart}">Add to Cart</button>
+    <button on:click="{() => {dispatch('delete', 's1')}}">Delete</button>
 </article>
